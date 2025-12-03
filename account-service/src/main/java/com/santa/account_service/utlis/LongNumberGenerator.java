72329ee6f916447a8ca7bb4722c3cc9849ed0445
santhosh <santhosh.003z@gmail.com>
2025-12-03 23:40:00 +0530

    added random id generator

---
+++ account-service/src/main/java/com/santa/account_service/utlis/LongNumberGenerator.java
@@ -0,0 +1,11 @@
+package com.santa.account_service.utlis;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class LongNumberGenerator {
+    public static String getID(int n){
+        return UUID.randomUUID().toString().replace("-", "").substring(0,n).toUpperCase();
+    }
+}
+
