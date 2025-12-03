9c883995348853187e7f7cd41d07b301f522a726
santhosh <santhosh.003z@gmail.com>
2025-12-03 18:51:30 +0530

    updated user with args annotation

--- auth-service/src/main/java/com/santa/auth_service/model/User.java
+++ auth-service/src/main/java/com/santa/auth_service/model/User.java
@@ -1,8 +1,10 @@
 package com.santa.auth_service.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.UUID;
@@ -11,10 +13,13 @@ import java.util.UUID;
 @Table(name = "users")
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(unique = true)
     private String email;
     private String passwordHash;
     private boolean isActive;
