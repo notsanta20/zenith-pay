2f4bb04125771ccdd29826795d5ce199ae8a9b10
santhosh <santhosh.003z@gmail.com>
2025-12-05 17:14:04 +0530

    added insufficiet balance exception

--- account-service/src/main/java/com/santa/account_service/advice/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/santa/account_service/advice/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.santa.account_service.advice;
 
 import com.santa.account_service.exception.ErrorResponse;
 import com.santa.account_service.exception.AccountNotFoundException;
+import com.santa.account_service.exception.InsufficientBalanceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleBookNotFound(AccountNotFoundException ex) {
         ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(),ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientBalance(InsufficientBalanceException ex) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_GATEWAY);
     }
 
     @ExceptionHandler(Exception.class)
