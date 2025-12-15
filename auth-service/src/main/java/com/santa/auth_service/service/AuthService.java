7c38301fc9b471444b765e4c2f235771bd124ece
santhosh <santhosh.003z@gmail.com>
2025-12-15 16:40:45 +0530

    add password hashing and jwt token for login

--- auth-service/src/main/java/com/santa/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/santa/auth_service/service/AuthService.java
@@ -1,16 +1,21 @@
 package com.santa.auth_service.service;
 
+import com.santa.auth_service.config.jwt.JwtService;
 import com.santa.auth_service.dto.LoginRequestDTO;
 import com.santa.auth_service.dto.LoginResponseDTO;
 import com.santa.auth_service.dto.RegisterRequestDTO;
 import com.santa.auth_service.dto.RegisterResponseDTO;
 import com.santa.auth_service.exception.EmailAlreadyExistsException;
-import com.santa.auth_service.exception.IncorrectPasswordException;
+import com.santa.auth_service.exception.UnAuthorizedException;
 import com.santa.auth_service.exception.UserNotFoundException;
 import com.santa.auth_service.model.User;
 import com.santa.auth_service.producer.ProfileCreationProducer;
 import com.santa.auth_service.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -19,23 +24,32 @@ import java.util.Optional;
 @Service
 public class AuthService {
 
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     private UserRepo userRepo;
     private ProfileCreationProducer profileCreationProducer;
+    AuthenticationManager authenticationManager;
+    private JwtService jwtService;
 
     @Autowired
-    public AuthService(UserRepo userRepo, ProfileCreationProducer profileCreationProducer) {
+    public AuthService(UserRepo userRepo,
+                       ProfileCreationProducer profileCreationProducer,
+                       AuthenticationManager authenticationManager,
+                       JwtService jwtService) {
         this.userRepo = userRepo;
         this.profileCreationProducer = profileCreationProducer;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     public RegisterResponseDTO registerUser(RegisterRequestDTO req) {
         Optional<User> existingUser = userRepo.findByEmail(req.getEmail());
 
-        if(existingUser.isPresent()) throw new EmailAlreadyExistsException(req.getEmail());
+        if (existingUser.isPresent()) throw new EmailAlreadyExistsException(req.getEmail());
 
         User user = User.builder()
                 .email(req.getEmail())
-                .passwordHash(req.getPassword())
+                .passwordHash(encoder.encode(req.getPassword()))
                 .isActive(false)
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
@@ -48,19 +62,27 @@ public class AuthService {
     }
 
     public LoginResponseDTO loginUser(LoginRequestDTO req) {
-        User user = userRepo.findByEmail(req.getEmail()).orElseThrow(()->new UserNotFoundException(req.getEmail()));
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(req.getEmail(), req.getPassword()));
 
-        if(!user.getPasswordHash().equals(req.getPassword())) throw new IncorrectPasswordException();
+        if (authentication.isAuthenticated()) {
+            User user = userRepo.findByEmail(req.getEmail()).orElseThrow(()->new UserNotFoundException(req.getEmail()));
 
-        LoginResponseDTO res = LoginResponseDTO.builder()
-                .accessToken("asdf")
-                .refreshToken("asdf")
-                .expiry("1m")
-                .userId(user.getId().toString())
-                .email(user.getEmail())
-                .isActive(user.isActive())
-                .build();
+            String token = jwtService.generateToken(user.getEmail());
 
-        return res;
+            LoginResponseDTO res = LoginResponseDTO.builder()
+                    .accessToken(token)
+                    .refreshToken("asdf")
+                    .expiry("2m")
+                    .userId(user.getId().toString())
+                    .email(user.getEmail())
+                    .isActive(user.isActive())
+                    .build();
+
+            return res;
+        } else {
+            throw new UnAuthorizedException();
+        }
+
     }
 }
