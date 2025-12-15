0decca933e28a93a14467cc4f53198c6eb748e25
santhosh <santhosh.003z@gmail.com>
2025-12-15 20:00:16 +0530

    add authentication filter

---
+++ api-gateway/src/main/java/com/santa/api_gateway/config/AuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.santa.api_gateway.config;
+
+import com.santa.api_gateway.config.jwt.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private RouteValidator validator;
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthenticationFilter(RouteValidator validator, JwtUtil jwtUtil) {
+        super(Config.class);
+        this.validator = validator;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new JwtException("missing auth token");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                jwtUtil.validateToken(authHeader);
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config{
+
+    }
+}
