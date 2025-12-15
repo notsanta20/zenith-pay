0d007de2b5ca6a9888d827dd8c391fb4c2ed3970
santhosh <santhosh.003z@gmail.com>
2025-12-15 20:01:39 +0530

    update api path

--- api-gateway/src/main/java/com/santa/api_gateway/config/RouteValidator.java
+++ api-gateway/src/main/java/com/santa/api_gateway/config/RouteValidator.java
@@ -1,14 +1,17 @@
 package com.santa.api_gateway.config;
 
-import org.springframework.http.server.ServerHttpRequest;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.function.Predicate;
 
+@Component
 public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
-            "api/auth/register",
-            "api/auth/login",
+            "/api/auth/register",
+            "/api/auth/login",
             "/eureka"
     );
 
