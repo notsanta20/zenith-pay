8c5d1183740deb0fa237c665f88876c0bb2bf7bd
santhosh <santhosh.003z@gmail.com>
2025-12-16 00:58:06 +0530

    updated api path

--- account-service/src/main/java/com/santa/account_service/controller/AccountController.java
+++ account-service/src/main/java/com/santa/account_service/controller/AccountController.java
@@ -8,9 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     private AccountService accountService;
