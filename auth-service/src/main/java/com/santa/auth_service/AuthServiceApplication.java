522b13c7397a1287e30f98455f57dc137475db80
santhosh <santhosh.003z@gmail.com>
2025-12-17 01:40:05 +0530

    remove unused imports

--- auth-service/src/main/java/com/santa/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/santa/auth_service/AuthServiceApplication.java
@@ -2,10 +2,10 @@ package com.santa.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
