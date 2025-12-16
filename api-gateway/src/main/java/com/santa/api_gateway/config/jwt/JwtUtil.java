90eb93a15db744adb70f31a6f974e13b3fa2c028
santhosh <santhosh.003z@gmail.com>
2025-12-17 01:34:45 +0530

    add exract email and claims methods

--- api-gateway/src/main/java/com/santa/api_gateway/config/jwt/JwtUtil.java
+++ api-gateway/src/main/java/com/santa/api_gateway/config/jwt/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.santa.api_gateway.config.jwt;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,21 +9,39 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
     private String secretKey;
+    private final SecretKey secret;
 
     public JwtUtil(@Value("${jwt.secret}") String secretKey){
         this.secretKey = secretKey;
+        this.secret = getKey();
     }
 
     public void validateToken(final String token) {
         Jwts
                 .parser()
-                .verifyWith(getKey())
+                .verifyWith(secret)
                 .build()
                 .parseSignedClaims(token);
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secret)
+                .build().parseSignedClaims(token).getPayload();
     }
 
     private SecretKey getKey() {
