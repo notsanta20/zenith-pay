0b375d7a6a3ceb55660a487c5882a2a9a162f0f2
santhosh <santhosh.003z@gmail.com>
2025-12-18 19:44:30 +0530

    update path and remove userId

--- transaction-service/src/main/java/com/santa/transaction_service/feign/AccountInterface.java
+++ transaction-service/src/main/java/com/santa/transaction_service/feign/AccountInterface.java
@@ -4,14 +4,11 @@ import com.santa.transaction_service.dto.TransactionRequestDTO;
 import com.santa.transaction_service.dto.TransactionResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("ACCOUNT-SERVICE")
 public interface AccountInterface {
-    @PutMapping("accounts/{accountId}/transact")
-    ResponseEntity<TransactionResponseDTO> updateAccountBalance(
-            @PathVariable String accountId,
-            @RequestBody TransactionRequestDTO req);
+    @PutMapping("api/accounts/transact")
+    ResponseEntity<TransactionResponseDTO> updateAccountBalance(@RequestBody TransactionRequestDTO req);
 }
