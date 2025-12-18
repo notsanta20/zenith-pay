eb844c0fbdde091cdd191e3532a4d01f1e3b1b0a
santhosh <santhosh.003z@gmail.com>
2025-12-18 19:35:23 +0530

    add find by account number

--- account-service/src/main/java/com/santa/account_service/repo/AccountRepo.java
+++ account-service/src/main/java/com/santa/account_service/repo/AccountRepo.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepo extends JpaRepository<Account, UUID> {
     Page<Account> findAllByUserId(UUID userId, Pageable pageable);
+
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
