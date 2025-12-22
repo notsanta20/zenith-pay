e60a8d74f00d7653d7d3bf651ca0c5004239e6f8
santhosh <santhosh.003z@gmail.com>
2025-12-22 23:21:58 +0530

    update: updated kyc status method to user status

--- auth-service/src/main/java/com/santa/auth_service/feign/ProfileInterface.java
+++ auth-service/src/main/java/com/santa/auth_service/feign/ProfileInterface.java
@@ -1,5 +1,6 @@
 package com.santa.auth_service.feign;
 
+import com.santa.auth_service.dto.UserStatusDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -7,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient("USER-SERVICE")
 public interface ProfileInterface {
-    @GetMapping("api/profile/kyc-status")
-    public boolean checkKycStatus(@RequestHeader("userId") String userId);
+    @GetMapping("api/profile/user-status")
+    UserStatusDTO getUserStatus(@RequestHeader("userId") String userId);
 
     @GetMapping("api/profile/username")
-    public String getUsername(@RequestHeader("userId") String userId);
+    String getUsername(@RequestHeader("userId") String userId);
 
 }
 
