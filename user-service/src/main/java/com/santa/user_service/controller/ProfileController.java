1b6e7999936793f3ae06468dc71dc31a0d2dbb26
santhosh <santhosh.003z@gmail.com>
2025-12-22 23:30:20 +0530

    feat: added update notifications api, update profile API return type and updated get kyc status API to user status

--- user-service/src/main/java/com/santa/user_service/controller/ProfileController.java
+++ user-service/src/main/java/com/santa/user_service/controller/ProfileController.java
@@ -1,15 +1,11 @@
 package com.santa.user_service.controller;
 
-import com.santa.user_service.dto.ProfileUpdateRequestDTO;
-import com.santa.user_service.dto.ProfileUpdateResponseDTO;
-import com.santa.user_service.model.Profile;
+import com.santa.user_service.dto.*;
 import com.santa.user_service.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/profile")
@@ -18,29 +14,41 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @Autowired
-    public ProfileController(ProfileService profileService){
+    public ProfileController(ProfileService profileService) {
         this.profileService = profileService;
     }
 
     @GetMapping("/me")
-    public ResponseEntity<Profile> getProfile(@RequestHeader("userId") String id){
-        Profile profile = profileService.getProfile(UUID.fromString(id));
+    public ResponseEntity<ProfileDTO> getProfile(@RequestHeader("userId") String userId) {
+        ProfileDTO profile = profileService.getProfile(userId);
         return new ResponseEntity<>(profile, HttpStatus.OK);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ProfileUpdateResponseDTO> updateProfile(@RequestBody ProfileUpdateRequestDTO req, @RequestHeader("userId") String userId){
-        ProfileUpdateResponseDTO updatedUser = profileService.updateUser(req,userId);
+    public ResponseEntity<ProfileUpdateResponseDTO> updateProfile(@RequestBody ProfileUpdateRequestDTO req, @RequestHeader("userId") String userId) {
+        ProfileUpdateResponseDTO updatedUser = profileService.updateUser(req, userId);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
-    @GetMapping("/kyc-status")
-    public boolean checkKycStatus(@RequestHeader("userId") String userId){
-        return profileService.getKycStatus(userId);
+    @GetMapping("/user-status")
+    public UserStatusDTO getUserStatus(@RequestHeader("userId") String userId) {
+        return profileService.getUserStatus(userId);
     }
 
     @GetMapping("/username")
-    public String getUsername(@RequestHeader("userId") String userId){
+    public String getUsername(@RequestHeader("userId") String userId) {
         return profileService.getUsername(userId);
+    }
+
+    @PutMapping("/update-security-notification")
+    public ResponseEntity<String> updateSecurityNotification(@RequestHeader("userId") String userId) {
+        profileService.updateSecurityNotification(userId);
+        return new ResponseEntity<>("Preference updated successfully", HttpStatus.OK);
+    }
+
+    @PutMapping("/update-general-notification")
+    public ResponseEntity<String> updateGeneralNotification(@RequestHeader("userId") String userId) {
+        profileService.updateGeneralNotification(userId);
+        return new ResponseEntity<>("Preference updated successfully", HttpStatus.OK);
     }
 }
