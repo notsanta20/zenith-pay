0280659581464beaff622cebbd1fa403001189bb
santhosh <santhosh.003z@gmail.com>
2025-12-22 23:32:33 +0530

    update: added kyc status and creation time felids and added constructor

--- user-service/src/main/java/com/santa/user_service/dto/ProfileDTO.java
+++ user-service/src/main/java/com/santa/user_service/dto/ProfileDTO.java
@@ -1,10 +1,12 @@
 package com.santa.user_service.dto;
 
+import com.santa.user_service.model.Profile;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +15,14 @@ public class ProfileDTO {
     private String full_name;
     private LocalDate dob;
     private String phone;
+    private boolean kyc_status;
+    private LocalDateTime created_at;
+
+    public ProfileDTO(Profile profile) {
+        this.full_name = profile.getFull_name();
+        this.dob = profile.getDob();
+        this.phone = profile.getPhone();
+        this.kyc_status = profile.isKyc_status();
+        this.created_at = profile.getCreated_at();
+    }
 }
